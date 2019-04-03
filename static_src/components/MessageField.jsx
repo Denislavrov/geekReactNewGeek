1272aa8650ebe199fa7034aaf4363ed958a27c8e
Denis Lavrov <d.lavrov@corp.mail.ru>
2019-04-03 21:38:28 +0300

    init commit

--- static_src/components/MessageField.jsx
+++ static_src/components/MessageField.jsx
@@ -3,36 +3,45 @@ import Message from './Message';
 
 export default class MessageField extends React.Component{
     state = {
-        messages: ['mess1', 'mess2', 'mess3']
+        messageList: [],
+        messages: {1: {'text': 'asdasdasd', 'sender': 'bot'}},
+        curId: 1,
     };
 
-    componentDidMount() {
-        const newMessages = [...this.state.messages, 'mess'];
-        setTimeout(()=>this.setState({ messages: newMessages }),1000);
-    }
+    // componentDidMount() {
+    //     const newMessages = [...this.state.messages, 'mess'];
+    //     setTimeout(()=>this.setState({ messages: newMessages }),1000);
+    // }
 
     componentDidUpdate(prevProps, prevState) {
-        console.log(prevState);
-        console.log(this.state);
-        console.log(this.state.messages);
-        if(prevState === this.state.messages[key % 2 === 0]) {
-             const newMessages = [...this.state.messages, 'mess'];
-             this.setState({ messages: newMessages });
+        const {messages, messageList} = this.state;
+        if(messages[messageList[messageList.length - 1]].sender === 'me') {
+            setTimeout(() => this.handlReply(),1000);
         }
-
+        // const newMessages = [...this.state.messages, 'mess'];
+        // setTimeout(() => this.setState({ messages: newMessages }), 1000);
     }
     handleClick = () => {
-        const newMessages = [...this.state.messages, 'myMessage'];
-        this.setState({ messages: newMessages });
+        const {messageList, messages, curId} = this.state;
+        const newMessageList = [...messageList, curId];
+        const newMessages = {...messages, [curId]: {text: 'Hello!', sender: 'me'}};
+        this.setState({ messages: newMessages, messageList: newMessageList, curId: curId + 1 });
+    };
+    handlReply = () => {
+        const {messageList, messages, curId} = this.state;
+        const newMessageList = [...messageList, curId];
+        const newMessages = {...messages, [curId]: {text: 'Hello motherfucker!', sender: 'bot'}};
+        this.setState({ messages: newMessages, messageList: newMessageList, curId: curId + 1 });
     };
 
     render() {
-        const messages = this.state.messages.map((message, index) =>
-            <Message key={ index } text={  message}/>
+        const {messageList, messages} = this.state;
+        const messageComponents = messageList.map((messageId, index) =>
+            <Message key={ index } text={ messages[messageId].text }/>
         );
 
         return (
-            <div>{ messages }
+            <div>{ messageComponents }
             <button onClick={ this.handleClick }>send message</button>
             </div>
         )
